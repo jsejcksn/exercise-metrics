375c525b49928652b5706aa04780909add241199
Jesse Jackson <jsejcksn@users.noreply.github.com>
2015-08-30 07:33:23 -0500

    Initial commit

---
+++ js/metrics.js
@@ -0,0 +1,118 @@
+// Exercise Metrics
+
+// Variables ----------
+
+var metrics = '';
+var date = ['x'];
+var distance = ['Distance (m)'];
+var pace = ['Pace (m/s)'];
+
+
+// Functions ----------
+
+function update() {
+  for (var i = 0; i < metrics.length; i++) {
+    if (metrics[i].activity == 'Run') {
+      date.push(metrics[i].date);
+      distance.push(metrics[i].distance);
+      pace.push(metrics[i].pace);
+    }
+  }
+  console.log(date.length + ' running, ' + metrics.length + ' total');
+}
+
+
+// EventListeners ----------
+
+
+
+
+// Execute ----------
+
+console.log('Exercise Metrics');
+
+// This should become an option for file upload or copy/paste input
+// From https://developers.google.com/web/updates/2015/03/introduction-to-fetch?hl=en
+fetch('metrics/exercise-metrics.json')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+        metrics = data;
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+setTimeout(update,500);
+
+var chartPace = c3.generate({
+  bindto: '#chart-pace',
+  data: {
+    x: 'x',
+//  xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+    columns: [
+//    ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+//    ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
+//    ['data1', 30, 200, 100, 400, 150, 250],
+//    ['data2', 130, 340, 200, 500, 250, 350],
+    ]
+  },
+  axis: {
+    x: {
+      type: 'timeseries',
+      tick: {
+        format: '%Y-%m-%d'
+      }
+    }
+  }
+});
+
+setTimeout(function () {
+  chartPace.load({
+    columns: [
+      date,
+      pace
+    ]
+  });
+}, 750);
+
+var chartDistance = c3.generate({
+  bindto: '#chart-distance',
+  data: {
+    x: 'x',
+//  xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+    columns: [
+//    ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+//    ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
+//    ['data1', 30, 200, 100, 400, 150, 250],
+//    ['data2', 130, 340, 200, 500, 250, 350],
+    ]
+  },
+  axis: {
+    x: {
+      type: 'timeseries',
+      tick: {
+        format: '%Y-%m-%d'
+      }
+    }
+  }
+});
+
+setTimeout(function () {
+  chartDistance.load({
+    columns: [
+      date,
+      distance
+    ]
+  });
+}, 750);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# exercise-metrics
+
+A quick way to chart progress from captured exercise metrics using [c3](//github.com/masayuki0812/c3/).
