0392be4311223a62a5bf68dbf1303169723e5c4a
Jesse Jackson <jsejcksn@users.noreply.github.com>
2015-08-30 16:09:56 -0500

    Change merge two charts into one chart

--- js/metrics.js
+++ js/metrics.js
@@ -55,8 +55,8 @@ fetch('metrics/exercise-metrics.json')
 
 setTimeout(update,500);
 
-var chartPace = c3.generate({
-  bindto: '#chart-pace',
+var chart = c3.generate({
+  bindto: '#chart',
   data: {
     x: 'x',
 //  xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
@@ -78,40 +78,10 @@ var chartPace = c3.generate({
 });
 
 setTimeout(function () {
-  chartPace.load({
+  chart.load({
     columns: [
       date,
-      pace
-    ]
-  });
-}, 750);
-
-var chartDistance = c3.generate({
-  bindto: '#chart-distance',
-  data: {
-    x: 'x',
-//  xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
-    columns: [
-//    ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
-//    ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
-//    ['data1', 30, 200, 100, 400, 150, 250],
-//    ['data2', 130, 340, 200, 500, 250, 350],
-    ]
-  },
-  axis: {
-    x: {
-      type: 'timeseries',
-      tick: {
-        format: '%Y-%m-%d'
-      }
-    }
-  }
-});
-
-setTimeout(function () {
-  chartDistance.load({
-    columns: [
-      date,
+      pace,
       distance
     ]
   });
