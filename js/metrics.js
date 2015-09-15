cc88ba23363817efb868444402fb7dff0dd9e16c
Jesse Jackson <jsejcksn@users.noreply.github.com>
2015-09-15 11:49:08 -0500

    Add local storage for tsv data

--- js/metrics.js
+++ js/metrics.js
@@ -11,16 +11,17 @@ var exMetrics = (function() {
     limit = parseInt(document.getElementById('limit-entries').value),
     pace = ['Pace (m/s)'],
     sampleTSV = 'date\tactivity\tduration\tdistance\tpace\n2015-05-01\tRun\t1440\t4700\t3.264\n2015-05-07\tRun\t1425\t4700\t3.298\n2015-05-12\tRun\t1593\t5000\t3.139\n2015-05-18\tRun\t1417\t4700\t3.317\n2015-05-22\tRun\t1661\t5207\t3.135\n2015-05-27\tRun\t1440\t4700\t3.264\n2015-05-31\tRun\t1385\t4700\t3.394\n2015-06-04\tRun\t1407\t4718\t3.353\n2015-06-07\tRun\t1389\t4718\t3.397\n2015-06-28\tRun\t1012\t3316\t3.277\n2015-07-09\tRun\t998\t3316\t3.323\n2015-07-12\tRun\t1500\t3015\t2.01\n2015-07-14\tRun\t1244\t4083\t3.282',
-    sampleJSON = tsvToJson(sampleTSV);
-  tsv = document.getElementById('data-input');
+    sampleJSON = tsvToJson(sampleTSV),
+    tsv = document.getElementById('data-input');
 
-  // function fillTSV() {
-  //   tsv.value = localStorage.exerciseDataTSV;
-  // }
+  function fillTSV() {
+    tsv.value = localStorage.exerciseDataTSV;
+  }
 
-  // function storeTSV() {
-  //   localStorage.setItem('exerciseDataTSV', tsv.value);
-  // }
+  function storeTSV() {
+    console.log('Storing TSV data');
+    localStorage.setItem('exerciseDataTSV', tsv.value);
+  }
 
   function tsvToJson(input) {
     var info = input.replace(/['"]/g, ''),
@@ -100,16 +101,15 @@ var exMetrics = (function() {
     }
   });
 
-  // tsv.addEventListener('change', storeTSV);
-  // if (!((localStorage.getItem('exerciseDataTSV') === null) && (localStorage.exerciseDataTSV === '') && (localStorage.exerciseDataTSV === undefined))) { // localStorage key exists and is not empty
-  //   fillTSV();
-  // }
+  tsv.value = sampleTSV;
+  tsv.addEventListener('keyup', storeTSV);
+  if (!(localStorage.getItem('exerciseDataTSV') === null || undefined || '' || 0)) { // localStorage key exists and is not empty
+    fillTSV();
+  }
 
   btn.addEventListener('click', function() {
     update(tsv.value);
   });
-
-  tsv.value = sampleTSV;
 
   setTimeout(function() {
     update(tsv.value);
